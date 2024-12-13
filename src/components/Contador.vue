b7d309e51fd458b5941f70bf8772ee3d1ccb1f98
Ale <alejandromidnez22@gmail.com>
2024-12-13 10:00:34 -0300

    Componente Contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,23 @@
+<template>
+<h1>Hola</h1>
+<p>{{ contador }}</p>
+<button @click="incrementar">incrementar</button>
+<button @click="noincrementar">noincrementar</button>
+</template>
+
+<script setup>
+import{ref} from 'vue'
+const contador = ref(0)
+const incrementar = () => {
+  contador.value++
+}
+const noincrementar = () => {
+  contador.value--
+}
+</script>
+
+<style scoped>
+p {
+  color: blue;
+}
+</style>
