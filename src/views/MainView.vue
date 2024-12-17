a2fff5352ef6c14ed8064864574aee29df1ad476
Ale <alejandromidnez22@gmail.com>
2024-12-17 16:26:42 -0300

    Header, texto, y guardado de cuenta

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,24 +1,29 @@
 <template>
-    <div class="container">
-      <Sidebar></Sidebar>
-      <div class="content">
-        <RouterView></RouterView>
-      </div>
+  <Header :mensaje="texto"></Header>
+  <div class="container">
+    <Sidebar></Sidebar>
+    <div class="content">
+      <input type="text" v-model="texto">
+      {{ texto }}
+      <RouterView></RouterView>
     </div>
-  </template>
-  
-  <script setup>
-  import Sidebar from '@/components/Sidebar.vue';
-  </script>
-  
-  <style>
-  .container {
-    display: flex;
-  }
-  
-  .content {
-    flex: 1;
-    padding: 20px;
-  }
-  </style>
-  
+  </div>
+</template>
+
+<script setup>
+import Sidebar from '@/components/Sidebar.vue';
+import Header from '@/components/Header.vue';
+import { ref } from 'vue';
+const texto = ref('');
+</script>
+
+<style>
+.container {
+  display: flex;
+}
+
+.content {
+  flex: 1;
+  padding: 20px;
+}
+</style>
