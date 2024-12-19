2ca1f53aec124b338600d5dd722a9f04affae30b
Ale <alejandromidnez22@gmail.com>
2024-12-19 11:04:25 -0300

    Footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <MainView></MainView>
+  <div id="app">
+    <Footer />
+  </div>
 </template>
 
 <script setup>
 import MainView from './views/MainView.vue';
 import { Comment } from 'vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <style scoped>
---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer class="footer">
+      <p>&copy; Pagina Falsa en vue todos los derechos reservados</p>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer',
+  };
+  </script>
+  
+  <style scoped>
+  .footer {
+    text-align: center;
+    padding: 20px;
+    background-color: #333;
+    color: white;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
+  </style>
+  
